39b6f01197532cdb4330922a5bd1b8471fd51a1b
Bogdan <bogdan.kurbetyev@gmail.com>
2018-08-21 12:51:47 +1000

    Add model relationships

--- app/Statistic.php
+++ app/Statistic.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Statistic extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeBatting($builder)
+    {
+        return $builder->where('type', 'batting');
+    }
+
+    public function scopeBowling($builder)
+    {
+        return $builder->where('type', 'bowling');
+    }
 }
