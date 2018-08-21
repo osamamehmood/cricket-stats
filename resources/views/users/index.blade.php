33688fa4e4aaa0fafa4885245a999bf64204bdbb
Osama <Osamamehmood786@gmail.com>
2018-08-21 13:05:57 +1000

    Create bowling stats table

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,10 +12,10 @@
                     <table class="table table-hover">
                         <thead>
                             <tr>
-                                <th scope="col">Number Of Runs</th>
-                                <th scope="col">Number of Bowls Faced</th>
-                                <th scope="col">Number of 4s</th>
-                                <th scope="col">Number of 6s</th>
+                                <th scope="col">Runs</th>
+                                <th scope="col">Balls Faced</th>
+                                <th scope="col">4s</th>
+                                <th scope="col">6s</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -35,6 +35,30 @@
                 <div class="card">
                   <div class="card-body">
                     <h2>Bowling</h2>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Overs</th>
+                                <th scope="col">Maidens</th>
+                                <th scope="col">Runs</th>
+                                <th scope="col">Wickets</th>
+                                <th scope="col">Wides</th>
+                                <th scope="col">No Balls</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach(auth()->user()->statistics()->Bowling()->get() as $bowling)
+                            <tr>
+                                <th>{{ $batting->overs_bowled }}</th>
+                                <th>{{ $batting->maiden_overs }}</th>
+                                <th>{{ $batting->runs_conceded }}</th>
+                                <th>{{ $batting->wickets_taken }}</th>
+                                <th>{{ $batting->wides_bowled }}</th>
+                                <th>{{ $batting->no_balls_bowled }}</th>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
                   </div>
                 </div>
