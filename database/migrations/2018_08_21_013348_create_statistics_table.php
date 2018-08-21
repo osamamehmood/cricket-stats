561481ff64b019301caed76bebd0ea9f8be1c9cc
Bogdan <bogdan.kurbetyev@gmail.com>
2018-08-21 12:18:59 +1000

    batting and bowling stat requirements added.

--- database/migrations/2018_08_21_013348_create_statistics_table.php
+++ database/migrations/2018_08_21_013348_create_statistics_table.php
@@ -15,6 +15,23 @@ class CreateStatisticsTable extends Migration
     {
         Schema::create('statistics', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('type')->default('batting'); // Can also be bowling
+            // Batting
+            $table->integer('number_of_runs');
+            $table->integer('number_of_bowls_faced');
+            $table->integer('number_of_4s');
+            $table->integer('number_of_6s');
+            $table->double('strike_rate');
+
+            // Bowling
+            $table->integer('overs_bowled');
+            $table->integer('maiden_overs');
+            $table->integer('runs_conceded');
+            $table->integer('wickets_taken');
+            $table->integer('economy');
+            $table->integer('wides_bowled');
+            $table->integer('no_balls_bowled');
+
             $table->timestamps();
         });
     }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    User dashboard
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-info d-flex justify-content-between">
+                    You do not have any stats, yet! <a class="alert-link" href="">Add Statistics</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
